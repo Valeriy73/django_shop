f171c196d2cba2d355627f2c828990f165a4345b
Valeriy Tur <turlera@ukr.net>
2016-09-08 23:15:01 +0300

    3 commit, setingurls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from main.models import *
+from django.http import HttpResponse, HttpRequest
+from django.template.loader import render_to_string
 
 # Create your views here.
+def home(request):
+	context= {
+		'title': 'Helloworld'
+	}
+	return HttpResponseren(render_to_string('index.html', context))
