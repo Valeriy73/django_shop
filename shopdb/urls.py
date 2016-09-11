4861ccdc72618d777e3d8ae04da48104f2526031
Valeriy Tur <turlera@ukr.net>
2016-09-11 20:24:02 +0300

    add static

--- shopdb/urls.py
+++ shopdb/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.home'),
     url(r'^item/(?P<alias>[^/]+)', 'main.views.item'),
     url(r'^(?P<alias>[^/]+)', 'main.views.get_category'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
